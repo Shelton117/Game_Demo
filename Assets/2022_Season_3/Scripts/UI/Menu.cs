e1a0598bd706b01404449862cf7d77487e8dc07b
Shelton <18927858896@163.com>
2022-07-20 16:26:11 +0800

    v 0.0.1 资源处理，基础脚本编写

---
+++ Assets/2022_Season_3/Scripts/UI/Menu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace _2022_Season_3.Scripts.UI
+{
+    public class Menu : MonoBehaviour
+    {
+        public void Start()
+        {
+            
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
